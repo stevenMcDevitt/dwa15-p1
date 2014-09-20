cf4ca92809799a99368e16a3111a66e697cc7a35
Steven McDevitt <smcdevitt@g.harvard.edu>
2014-09-20 15:24:19 -0400

    Initial Baseline for P1

---
+++ index.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html class="no-js">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>DWA15 - Steven McDevitt</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href='http://fonts.googleapis.com/css?family=Maven+Pro' rel='stylesheet' type='text/css'>
+        <link href="css/normalize.css" rel="stylesheet" >
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/main.css" rel="stylesheet">
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+    </head>
+
+    <body>
+        
+        <header>
+            <div class="container">
+                <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+                    <div class="container-fluid">
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-100">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                            <a class="navbar-brand" href="index.php">DWA15</a>
+                        </div>
+                        <div class="collapse navbar-collapse" id="navbar-collapse-100">
+                            <ul class="nav navbar-nav">
+                                <li class=active><a href="index.php">P1 - Portfolio</a></li>
+                                <li><a href="p2.php">P2 - TBD</a></li>
+                                <li><a href="p3.php">P3 - TBD</a></li>
+                                <li><a href="p4.php">P4 - TBD</a></li>
+                            </ul>
+                            <form class="navbar-form navbar-right" role="search">
+                                <div class="input-group custom-search-form">
+                                    <input type="text" class="form-control">
+                                        <span class="input-group-btn">
+                                            <button class="btn btn-default" type="button">
+                                                <span class="glyphicon glyphicon-search"></span>
+                                            </button>
+                                        </span>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </header>
+
+        <div class="container">
+            <div class="row">
+                <div class="jumbotron col-md-12">
+                    <h1>Steven McDevitt</h1>
+                    <h1>Dynamic Web Applications Portfolio
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>A Brief Description of Me<img class="img-rounded img-responsive image-right hidden-xs hidden-sm" src="img/mcdevitt01.jpg"></h1>
+                    <p>My professional passion is focused on leveraging information technology, in the form of people, services, process and planning to support the mission of an organization.  This mission centric sensibility has been developed over the last two decades as I have had the good fortune and privilege of serving within organizations with important and interesting missions.</p>
+                    <p>My experience includes technical and leadership positions in the federal government (Federal Emergency Management Agency (FEMA), Executive Office of the President, and the Board of Governors of the Federal Reserve System), large non-profit organization (Archdiocese of Boston) and in small (InteliData Technologies) and large (Bank One, Booz Allen Hamilton) corporate environments.</p>
+                    <p>Currently I serve in the role of Director of Information Technology for the Archdiocese of Boston with the specific purpose of right sizing and modernizing the enterprise management, resource planning on information technology infrastructure of the organization.  With a small team and a small budget, we transformed the operating model to improve the mission focus of the organization.</p>
+                    <p><a href="http://www.facebook.com/smcdevitt">My Facebook Profile</a></p>
+                    <p><a href="http://www.linkedin.com/in/stevenmcdevitt/">My LinkedIn Profile</a></p>
+                    <p class="bottom-sep"></p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                   <h1>Reason for Taking This Course</h1>
+                    <p>I truly enjoy hands-on appliction development and I am seeking to develop a set of current generation skills for my next opportunity.</p>
+                    <p>I have some interesting ideas on improvments to the management and extension of reach for large non-profit organizations.  Next I will be working with a group of like-minded people who wish to extend the digital, social and ontological reach of how we engage people.</p>
+                    <p class="bottom-sep"></p>
+                </div>
+
+                <div class="col-md-6 col-sm-12 col-xs-12">
+                    <h1>Programming Experience</h1>
+                    <p>1970's - Z80 and 8080 Assembly Language on CP/M.</p>
+                    <p>1980's - Macro-11 and C on RSX-11M and UNIX on PDP-11 and VAX.</p>
+                    <p>1990's - A little PowerBuilder, Visual Basic, Java on various Microsoft and UNIX platforms.  Mostly technical architecture and project management.</p>
+                    <p>2000's - A miniscule amount of Java and Perl.  Mostly IT leadership positions.</p>
+                    <p class="bottom-sep"></p>
+                    <h1>My Platform</h1>
+                    <p>MacBook Pro 15</p>
+                    <p>Sublime Text, Git and Coffee (the kind you drink)</p>
+                    <p>Also, began using HTML5 Boiler Plate and Bootstrap</p>
+                    <p class="bottom-sep"></p>
+                </div>
+            </div>
+
+            <div class="row sitefooter">
+                <div class="col-md-4 sitefooter"></div>
+                <div class="col-md-8 sitefooter"></div>
+            </div>
+
+        </div>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/main.js"></script>
+
+    </body>
+</html>
